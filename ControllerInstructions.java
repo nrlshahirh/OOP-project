e115f8b12b04cd5e542d2f93a873eddc4cd99a99
codelessadam <87315445+codelessadam@users.noreply.github.com>
2021-07-13 22:36:10 +0800

    Create ControllerInstructions.java

---
+++ ControllerInstructions.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class ControllerInstructions {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void backButton(ActionEvent event) throws IOException {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/MainPage.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+
+}
